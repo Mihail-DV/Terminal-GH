3c4877394488690bc24fdf7eb992db0528f1662f
Mihail-DV <76704615+Mihail-DV@users.noreply.github.com>
2023-05-29 23:20:27 +0300

    Ïnitial Commit

---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by Михаил Данилин on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
